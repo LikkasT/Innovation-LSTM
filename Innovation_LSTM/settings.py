cd1ed75c2dab9cc6d697f6ec387994a114cf9203
Likkas <327084701@qq.com>
2024-05-06 17:30:26 +0800

    把之前写的登录放进来了

--- Innovation_LSTM/settings.py
+++ Innovation_LSTM/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Innovation_LSTM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CSshop',
+        'USER': 'root',
+        'PASSWORD': 'Szc#030609',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
